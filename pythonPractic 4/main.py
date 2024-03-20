c9072aabab61c9918c267e3c19a135bffb0c704b
Savchuk Volodymyr <99960932+NickNameSOON@users.noreply.github.com>
2024-03-20 05:29:03 +0200

    Update main.py

--- pythonPractic 4/main.py
+++ pythonPractic 4/main.py
@@ -1,45 +1,80 @@
-class Animal:
-    def __init__(self, name, habitat, diet, blood_type):
+class Animal: #Визначає основні атрибути тварини: name (ім'я) та habitat (середовище проживання).
+    def __init__(self, name, habitat):
         self.name = name
         self.habitat = habitat
-        self.diet = diet
-        self.blood_type = blood_type
+
+    def __str__(self):
+        return f"{self.name} that lives in the {self.habitat}."
+
+
+class Herbivore(Animal):    #Наслідується від класу Animal
+    def __init__(self, name, habitat):
+        super().__init__(name, habitat)
+        self.diet = "herbivore"
+
+
+class Carnivore(Animal):    #Наслідується від класу Animal
+    def __init__(self, name, habitat):
+        super().__init__(name, habitat)
+        self.diet = "carnivore"
+
+
+class WarmBlooded(Animal):  #Наслідується від класу Animal
+    def __init__(self, name, habitat):
+        super().__init__(name, habitat)
+        self.blood_type = "warm blooded"
+
+
+class ColdBlooded(Animal):  #Наслідується від класу Animal
+    def __init__(self, name, habitat):
+        super().__init__(name, habitat)
+        self.blood_type = "cold blooded"
+
+
+class Artiodactyls(Herbivore, WarmBlooded):     #Наслідує від класів Herbivore та WarmBlooded.
+    def __init__(self, name, habitat):
+        super().__init__(name, habitat)
 
     def __str__(self):
         return f"{self.name} is a {self.diet} that lives in the {self.habitat}. It is {self.blood_type}."
 
 
-class Herbivore(Animal):
-    def __init__(self, name, habitat, blood_type):
-        super().__init__(name, habitat, "herbivore", blood_type)
+class Bear(Herbivore, Carnivore, WarmBlooded):      #Наслідує від класів Herbivore, Carnivore та WarmBlooded.
+    def __init__(self, name, habitat):
+        super().__init__(name, habitat)
+
+    def __str__(self):
+        return f"{self.name} is a {self.diet} that lives in the {self.habitat}. It is {self.blood_type}."
 
 
-class Carnivore(Animal):
-    def __init__(self, name, habitat, blood_type):
-        super().__init__(name, habitat, "carnivore", blood_type)
+class Snakes(Carnivore, ColdBlooded):       #Наслідує від класів Carnivore та ColdBlooded.
+    def __init__(self, name, habitat):
+        super().__init__(name, habitat)
 
 
-class Artiodactyl(Herbivore):
-    def __init__(self, name, habitat, blood_type):
-        super().__init__(name, habitat, blood_type)
+class PoisonousSnake(Snakes):       #Наслідуються від класу Snakes.
+    def __init__(self, name, habitat):
+        super().__init__(name, habitat)
+        self.isPoisonous = True         #Визначається атрибут отруйність
+
+    def __str__(self):
+        return f"{self.name} is a {self.diet} that lives in the {self.habitat}. It is {self.blood_type}."
 
 
-class Bear(Carnivore):
-    def __init__(self, name, habitat, blood_type):
-        super().__init__(name, habitat, blood_type)
+class NonPoisonousSnake(Snakes):        #Наслідуються від класу Snakes.
+    def __init__(self, name, habitat):
+        super().__init__(name, habitat)
+        self.isPoisonousSnake = False   #Визначається атрибут отруйність
+
+    def __str__(self):
+        return f"{self.name} is a {self.diet} that lives in the {self.habitat}. It is {self.blood_type}."
 
 
-class Snake(Carnivore):
-    def __init__(self, name, habitat, blood_type, poisonous):
-        super().__init__(name, habitat, blood_type)
-        self.poisonous = poisonous
-
-
-cow = Artiodactyl("Cow", "Grasslands", "warm-blooded")
-brown_bear = Bear("Brown Bear", "Forests", "warm-blooded")
-grizzly_bear = Bear("Grizzly Bear", "Mountains", "warm-blooded")
-cobra = Snake("Cobra", "Deserts", "cold-blooded", True)
-adder = Snake("Adder", "Forests", "cold-blooded", False)
+cow = Artiodactyls("Cow", "Grasslands")
+brown_bear = Bear("Brown Bear", "Forests")
+grizzly_bear = Bear("Grizzly Bear", "Mountains")
+cobra = PoisonousSnake("Cobra", "Deserts")
+adder = NonPoisonousSnake("Adder", "Forests")
 
 print(cow)
 print(brown_bear)
