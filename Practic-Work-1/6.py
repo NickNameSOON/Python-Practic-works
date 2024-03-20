2f2938e92a3ac35c98bd00df94b163909502d339
Savchuk Volodymyr <99960932+NickNameSOON@users.noreply.github.com>
2024-03-20 05:28:16 +0200

    Update 6.py

--- Practic-Work-1/6.py
+++ Practic-Work-1/6.py
@@ -1,27 +1,16 @@
 def calculate_y(a, b, n, x, m):
-  
-  # Обчислення (a + b)^n
   power_ab = (a + b) ** n
-
-  # Обчислення x^m
   power_x = x ** m
-
-  # Обчислення b^(n - n)
   power_b = b ** (n - n)
-
-  # Обчислення знаменника
   denominator = 1 + x / (power_x + power_b)
-
-  # Обчислення Y
   y = power_ab / denominator
-
   return y
 
-a = 2
-b = 3
-n = 4
-x = 5
-m = 2
+a = float(input("Enter a number a: "))
+b = float(input("Enter a number b: "))
+n = float(input("Enter a number n: "))
+x = float(input("Enter a number x: "))
+m = float(input("Enter a number m: "))
 
 
 y = calculate_y(a, b, n, x, m)
